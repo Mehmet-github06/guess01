975ef4b958fcf714fb6328666404bfae9c04bdb3
mehmet dogan <mehmetseymen91@gmail.com>
2023-11-09 16:30:01 +0300

    html bitti

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ let input = document.querySelector(".input");
 let text  = document.querySelector(".text");
 let fist  = document.querySelector(".first");
 let second = document.querySelector(".second");
-
+let text2 = document.querySelector(".text2");
+let testNumber = document.querySelector(".test-number");
 
 let random = Math.floor(Math.random() *101);
 console.log(random);
@@ -18,12 +19,22 @@ check.addEventListener("click", () => {
         
         if (guess === random) {
             text.textContent = "Tebrikler, doğru tahmin ettiniz! 👏";
+            text.textContent=`Tebrikler ${+(testNumber.textContent)+1} tahminde bildiniz!` ;
         } else if (guess < random) {
-            text.textContent = "Daha büyük bir sayı deneyin.";
+            text.textContent = " 👆 Sayınızı Arttırın 👆";
             fist.textContent=guess
+            text2.style.display="block";
+            testNumber.textContent++;
         } else {
-            text.textContent = "Daha küçük bir sayı deneyin.";
+            text.textContent = "👇 Sayınızı Azaltın 👇";
             second.textContent=guess;
+            text2.style.display="block";
+            testNumber.textContent++;
         }
     }
 });
+document.addEventListener("keydown", function(e){
+    if(e.key ==="Enter"){
+        check.click()
+    }
+})
